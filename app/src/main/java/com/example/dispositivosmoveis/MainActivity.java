10c0e4859fbb0fc116d6f8c0ccacd5ab5a897380
Maria <tonhapaino25@gmail.com>
2026-05-31 16:41:25 -0300

    atividade contador

--- app/src/main/java/com/example/dispositivosmoveis/MainActivity.java
+++ app/src/main/java/com/example/dispositivosmoveis/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.dispositivosmoveis;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +14,21 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    int i=0;
+    TextView tv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        Button b=findViewById(R.id.button);
+        tv =  findViewById(R.id.contador);
+        b.setOnClickListener (v -> {
+            i++;
+            tv.setText(Integer.toString(i));
+
         });
     }
+
 }
