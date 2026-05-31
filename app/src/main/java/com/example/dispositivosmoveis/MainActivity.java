4051cdc84eda5fcf7c301bd6379cc8b4effc9b4d
Maria <tonhapaino25@gmail.com>
2026-05-31 17:42:08 -0300

    Aplicação Sorteio de números aleatórios

--- app/src/main/java/com/example/dispositivosmoveis/MainActivity.java
+++ app/src/main/java/com/example/dispositivosmoveis/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.dispositivosmoveis;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +13,44 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
+
+    Button sortear;
+    TextView resultado;
+    EditText numero1;
+    EditText numero2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+         sortear = findViewById(R.id.sortear);
+         resultado = findViewById(R.id.resultado);
+         numero1 = findViewById(R.id.numero1);
+         numero2 = findViewById(R.id.numero2);
+
+        sortear.setOnClickListener(this::realizaSorteio);
     }
+
+    private void realizaSorteio(View view) {
+        try {
+            int n1 = Integer.parseInt(numero1.getText().toString());
+            int n2 = Integer.parseInt(numero2.getText().toString());
+
+            int res = new Random().nextInt((n2 - n1)+1)+n1;
+
+            resultado.setText("Resultado: " + res);
+
+        } catch (Exception e) {
+
+            new AlertDialog.Builder(this).setMessage("Informe dois números para poder sortear!").show();
+
+        }
+
+    }
+
+
 }
